bed4b011bf8fef6721663188695cf37d8ede00c0
Vinay Raikar <rockraikar@gmail.com>
2019-10-31 02:04:00 +0530

    Add answer option component

---
+++ src/components/AnswerOption.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let text;
+  export let selected;
+  export let correct;
+
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<style>
+  li {
+    list-style-type: none;
+    border-radius: 0.5em;
+    padding: 0.5em;
+    border: 1px solid #aaa;
+    margin: 0.25em;
+  }
+  .answered-icon {
+    display: inline-block;
+    width: 1em;
+  }
+</style>
+
+<li on:click={() => dispatch('click')}>
+  <span class="answered-icon">
+    {#if correct === true}
+      ✓
+    {:else if selected && correct === false}✘{:else}&nbsp;{/if}
+  </span>
+  {text}
+</li>
---
+++ src/components/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let name;
+  import AnswerOption from "./AnswerOption.svelte";
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<ul>
+  <AnswerOption
+    text="selected={false} correct={false}"
+    on:click={() => console.log('clicked')}
+    selected={false}
+    correct={false} />
+  <AnswerOption
+    text="selected={false} correct={true}"
+    selected={false}
+    correct={true} />
+  <AnswerOption
+    text="selected={false} correct={null}"
+    selected={false}
+    correct={null} />
+
+  <AnswerOption
+    text="selected={true} correct={false}"
+    selected={true}
+    correct={false} />
+  <AnswerOption
+    text="selected={true} correct={true}"
+    selected={true}
+    correct={true} />
+  <AnswerOption
+    text="selected={true} correct={null}"
+    selected={true}
+    correct={null} />
+
+</ul>

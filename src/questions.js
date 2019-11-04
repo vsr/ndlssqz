5bc23eb8b35efbfb0ce446c27090a381aaa7c093
Vinay Raikar <rockraikar@gmail.com>
2019-11-05 01:12:40 +0530

    Add some questions

--- src/questions.js
+++ src/questions.js
@@ -1,9 +1,22 @@
 import { sendRequest } from "./xhr";
 
+const questionFiles = [
+  "/data/inventions.json",
+  "/data/geography.json",
+  "/data/physics.json"
+];
+let qFiles = [];
+
+const randomQuestionFile = () => {
+  if (qFiles.length === 0) {
+    qFiles = [...questionFiles];
+  }
+  return qFiles.splice(Math.floor(Math.random() * qFiles.length), 1)[0];
+};
+
 export const getNewQuestions = () => {
   return new Promise((resolve, reject) => {
-    sendRequest("/data/q-a.json", (err, req) => {
-      console.log("sendRequest", err, req);
+    sendRequest(randomQuestionFile(), (err, req) => {
       if (err) return reject(err);
       const questions = JSON.parse(req.responseText);
       resolve(questions);
